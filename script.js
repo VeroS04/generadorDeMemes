d6754938a97ddbae777e46b95bfc5044e7f937d6
Lucila Paiva <lucila.paiva@gmail.com>
2022-06-28 00:04:40 -0300

    Cambios

--- script.js
+++ script.js
@@ -338,7 +338,6 @@ sinContorno.addEventListener("click", () => {
   contornoOscuro.classList.remove("btnContornoSelected");
 });
 
-<<<<<<< HEAD
 contornoClaro.addEventListener("click", () => {
   topText.style.textShadow = "2px 0 0 white, -2px 0 0 white, 0 2px 0 white, 0 -2px 0 white ";
   bottomText.style.textShadow = "2px 0 0 white, -2px 0 0 white, 0 2px 0 white, 0 -2px 0 white";
@@ -354,24 +353,6 @@ contornoOscuro.addEventListener("click", () => {
   sinContorno.classList.remove("btnContornoSelected");
   contornoClaro.classList.remove("btnContornoSelected");
 });
-=======
-contornoClaro.addEventListener('click',() =>{
-topText.style.textShadow = '-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white';
-bottomText.style.textShadow = '-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white';
-contornoClaro.classList.add('btnContornoSelected');
-sinContorno.classList.remove('btnContornoSelected');
-contornoOscuro.classList.remove('btnContornoSelected')
-
-})
-
-contornoOscuro.addEventListener('click',() =>{
-topText.style.textShadow = '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black';
-bottomText.style.textShadow = '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black';
-contornoOscuro.classList.add('btnContornoSelected');
-sinContorno.classList.remove('btnContornoSelected');
-contornoClaro.classList.remove('btnContornoSelected');
-})
->>>>>>> b02fbed531d25d67c88035355c48352f7a9cbd19
 
 
 const espacioInput = document.getElementById("espacioInput");
